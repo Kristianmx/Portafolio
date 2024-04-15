364894c981d6295aacb173a24f7052836fc64d73
Kristiamnx <rivaskristopher24@gmail.com>
2024-04-14 21:25:59 -0500

    Funcionalidad al 70%

--- App/appEntrada.js
+++ App/appEntrada.js
@@ -1,5 +1,6 @@
 // --- Selects --- //
 const intro = document.getElementById("intro");
+const inicio = document.getElementById("inicio");
 const framer =document.getElementById("Frame-primary");
 const textBanner =document.getElementById("text-banner");
 const TB1 =document.getElementById("TB1");
@@ -8,39 +9,50 @@ const TB21= document.getElementById("TB21");
 const h1N= document.getElementById("h1N");
 const h2T= document.getElementById("h2T");
 
+
 // --- Events de entrada --- //
 document.addEventListener("DOMContentLoaded",()=>{
  framer.style.display="none";
  
+//intro.style.display="none";
   setTimeout(()=>{
    intro.style.animation="intro 1s ease-out";
-    framer.style.animation="introP 650ms ease-out";
+     txtl.style.display="none";
+   txtr.style.display="none";
+   framer.style.animation="introP 650ms ease-out";
+  
    framer.style.border="2px solid #111111";
    TB1.style.animation="h1T 650ms ease-out";
    TB2.style.animation="h2T 650ms ease-out";
    TB21.style.animation="h2T 650ms ease-out";
-  h1N.style.animation="h1N 650ms ease-out";
+   h1N.style.animation="h1N 650ms ease-out";
  },960);
  setTimeout(()=>{
-  intro.style.display="none";
+   intro.style.display="none";
+ 
 },1968);
 
   setTimeout(()=>{
-     intro.style.display="none";
-    
-     framer.style.display="block";
+   intro.style.display="none"; 
+   framer.style.display="block";
+  
   },2969);
- setTimeout(()=>{
- 
- framer.style.animation="zoom 1s ease-out";
- TB1.style.animation="h1TV 1s ease-out";
- TB2.style.animation="h2TV 1s ease-out";
- TB21.style.animation="h2TV 1s ease-out";
- textBanner.style.animation="tamanioCT 1s ease-out";
-  framer.style.border="0px solid black";
- },4500);
- setTimeout(()=>{
+  setTimeout(()=>{
+  txtl.style.display="block";
+    txtr.style.display="block";
+  },3400);
 
+ setTimeout(()=>{
+  
+   framer.style.animation="zoom 1s ease-out";
+   TB1.style.animation="h1TV 1s ease-out";
+   TB2.style.animation="h2TV 1s ease-out";
+   TB21.style.animation="h2TV 1s ease-out";
+   textBanner.style.animation="tamanioCT 1s ease-out";
+   framer.style.border="0px solid black";
+},4500);
+ setTimeout(()=>{
+  inicio.style.width="100%";
   framer.style.top="0%";
   framer.style.left="0%";
   framer.style.width="100%";
--- App/appFuncionalidad.js
+++ App/appFuncionalidad.js
@@ -2,27 +2,76 @@
 const contentLeft =document.getElementById("content-r");
 const banner =document.getElementById("content-banner");
 const contentRight =document.getElementById("content-l");
+const RCon = document.getElementById("RCon");
+const LCon = document.getElementById("LCon");
+const LC = document.getElementById("Lc");
+const btnl = document.getElementById("btnl");
+const btnr = document.getElementById("btnr");
+const txtl = document.getElementById("txtl");
+const txtr = document.getElementById("txtr");
+const conLeft = document.getElementById("conLeft");
+const conRight = document.getElementById("conRight");
+
 //contentLeft = 33%
 //   banner   = 34%
 //contentRight= 33%
 
 // --- Events ---//
-contentLeft.addEventListener("click",()=>{
+RCon.addEventListener("click",()=>{
   contentLeft.style.animation="contentExpander 1s ease-out";
-  contentRight.style.animation="contentContract 1s ease-out"
+  contentRight.style.animation="contentContract 1s ease-out";
+  LC.style.display="none";
+  btnr.style.display="block";
+ txtr.style.display="none";
+ conRight.style.display="flex";
   setTimeout(()=>{
     contentRight.style.width="0%";
     contentLeft.style.width="66%";
-  },1002)
+    contentRight.style.display="none";
+  },1001);
+});
+btnr.addEventListener("click",()=>{
+  contentLeft.style.animation="contentContractExtender 1s ease-out";
+  contentRight.style.animation="contentExpanderExtender 1s ease-out";
+  RCon.style.display="flex";
+  LC.style.display="flex";
+  btnr.style.display="none";
+  conRight.style.display="none";
+  //setTimeout(()=>{
+    contentRight.style.width="33%";
+    contentLeft.style.width="33%";
+    contentRight.style.display="flex";
+    txtr.style.display="block";
 
+ // },1000);
 });
 
-contentRight.addEventListener("click",()=>{
+LCon.addEventListener("click",()=>{
   contentRight.style.animation="contentExpander 1s ease-out";
-  contentLeft.style.animation="contentContract 1s ease-out"
+  contentLeft.style.animation="contentContract 1s ease-out";
+  txtl.style.display="none";
+  RCon.style.display="none";
+  btnr.style.display="none";
+  btnl.style.display="block";
+  conLeft.style.display="flex";
   setTimeout(()=>{
     contentLeft.style.width="0%";
     contentRight.style.width="66%";
-  },1002)
+    contentLeft.style.display="none";
+  
+  },1001);
+});
+btnl.addEventListener("click",()=>{
+  contentRight.style.animation="contentContractExtender 1s ease-out";
+  contentLeft.style.animation="contentExpanderExtender 1s ease-out";
+  RCon.style.display="flex";
+  btnl.style.display="none";
+  conLeft.style.display="none";
+ // setTimeout(()=>{
+    contentLeft.style.width="33%";
+    contentRight.style.width="33%";
+    contentLeft.style.display="flex";
+    txtl.style.display="block";
 
+ // },1000);
 });
