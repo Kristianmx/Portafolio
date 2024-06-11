99c25ddc8f418351513d7c5a4379fd0ee510e6f2
Kristianmx <rivaskristopher24@gmail.com>
2024-06-11 10:34:43 -0500

    estructura peque

--- App/appEntrada.js
+++ App/appEntrada.js
@@ -15,53 +15,53 @@ document.addEventListener("DOMContentLoaded",()=>{
  framer.style.display="none";
  
 //intro.style.display="none";
-  setTimeout(()=>{
-   intro.style.animation="intro 1s ease-out";
-     txtl.style.display="none";
-   txtr.style.display="none";
-   framer.style.animation="introP 650ms ease-out";
+//   setTimeout(()=>{
+//    intro.style.animation="intro 1s ease-out";
+//      txtl.style.display="none";
+//    txtr.style.display="none";
+//    framer.style.animation="introP 650ms ease-out";
   
-   framer.style.border="2px solid #111111";
-   TB1.style.animation="h1T 650ms ease-out";
-   TB2.style.animation="h2T 650ms ease-out";
-   TB21.style.animation="h2T 650ms ease-out";
-   h1N.style.animation="h1N 650ms ease-out";
- },960);
- setTimeout(()=>{
-   intro.style.display="none";
+//    framer.style.border="2px solid #111111";
+//    TB1.style.animation="h1T 650ms ease-out";
+//    TB2.style.animation="h2T 650ms ease-out";
+//    TB21.style.animation="h2T 650ms ease-out";
+//    h1N.style.animation="h1N 650ms ease-out";
+//  },960);
+//  setTimeout(()=>{
+    intro.style.display="none";
  
-},1968);
+// },1968);
 
-  setTimeout(()=>{
-   intro.style.display="none"; 
+//   setTimeout(()=>{
+    intro.style.display="none"; 
    framer.style.display="block";
   
-  },2969);
-  setTimeout(()=>{
-  txtl.style.display="block";
-    txtr.style.display="block";
-  },3400);
+//   },2969);
+//   setTimeout(()=>{
+//   txtl.style.display="block";
+//     txtr.style.display="block";
+//   },3400);
 
- setTimeout(()=>{
+//  setTimeout(()=>{
   
-   framer.style.animation="zoom 1s ease-out";
-   TB1.style.animation="h1TV 1s ease-out";
-   TB2.style.animation="h2TV 1s ease-out";
-   TB21.style.animation="h2TV 1s ease-out";
-   textBanner.style.animation="tamanioCT 1s ease-out";
-   framer.style.border="0px solid black";
-},4500);
- setTimeout(()=>{
-  inicio.style.width="100%";
+//    framer.style.animation="zoom 1s ease-out";
+//    TB1.style.animation="h1TV 1s ease-out";
+//    TB2.style.animation="h2TV 1s ease-out";
+//    TB21.style.animation="h2TV 1s ease-out";
+//    textBanner.style.animation="tamanioCT 1s ease-out";
+//    framer.style.border="0px solid black";
+// },4500);
+//  setTimeout(()=>{
+   inicio.style.width="100%";
   framer.style.top="0%";
   framer.style.left="0%";
   framer.style.width="100%";
   framer.style.height="100%";
-  textBanner.style.height="25%";
-  TB1.style.fontSize="3vw";
-  TB2.style.fontSize="2vw";
-  TB21.style.fontSize="2vw";
-  h2T.style.marginTop="2rem"
- },5500);
+//   textBanner.style.height="25%";
+//   TB1.style.fontSize="3vw";
+//   TB2.style.fontSize="2vw";
+//   TB21.style.fontSize="2vw";
+//   h2T.style.marginTop="2rem"
+//  },5500);
 })
 
